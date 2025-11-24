7e8e997191f4281b31ef8f46cc7faea4a3f136b6
dhyeye <153886201+dhyeyppatel@users.noreply.github.com>
2025-11-24 18:11:26 +0530

    Update URL in pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -734,7 +734,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ 📜', url='https://github.com/Silicon-Developer/Auto-Filter-Bot.git'),
+            InlineKeyboardButton('ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ 📜', url='https://t.me/+NHSZps-PCTQzNTE1'),
             InlineKeyboardButton('⇋ ʙᴀᴄᴋ ⇋', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
